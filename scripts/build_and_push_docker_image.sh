1c7bbb2048bf189209d188a8d8db291a51bfc2db
Melroy van den Berg <melroy@melroy.org>
2025-11-29 17:21:31 +0100

    Cleanup shell script

--- scripts/build_and_push_docker_image.sh
+++ scripts/build_and_push_docker_image.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 docker build -t registry.melroy.org/melroy/cidr-watcher/cidr-watcher:latest .
-
-# Publish to both GitLab Registry
 docker push registry.melroy.org/melroy/cidr-watcher/cidr-watcher:latest
